e0ba8cbc805617025bb9595d2bf1f45e14b5934a
Danin Na <dnami001@ucr.edu>
2024-01-16 01:18:41 -0800

    Form Submition - React Form Hook - Done

--- wheelwise2/app/leads/new/page.tsx
+++ wheelwise2/app/leads/new/page.tsx
@@ -2,42 +2,52 @@
 
 import { Button, TextField } from "@radix-ui/themes";
 import { RiUserStarLine, RiUserStarFill } from "react-icons/ri";
+import { useForm } from "react-hook-form";
 import React from "react";
 
-const NewLead = () => {
+interface NewLeadForm {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+}
+
+const NewLeadPage = () => {
+  const { register, handleSubmit } = useForm<NewLeadForm>();
+
   return (
-    <>
+    <form
+      className="flex gap-3"
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
       <TextField.Root>
         <TextField.Slot>
           <RiUserStarLine />
         </TextField.Slot>
-        <TextField.Input placeholder="First Name" />
+        <TextField.Input placeholder="First Name" {...register("firstName")} />
       </TextField.Root>
-
       <TextField.Root>
         <TextField.Slot>
           <RiUserStarFill />
         </TextField.Slot>
-        <TextField.Input placeholder="Last Name" />
+        <TextField.Input placeholder="Last Name" {...register("lastName")} />
       </TextField.Root>
-
       <TextField.Root>
         <TextField.Slot>
           <RiUserStarLine />
         </TextField.Slot>
-        <TextField.Input placeholder="Phone Number" />
+        <TextField.Input placeholder="Phone Number" {...register("phone")} />
       </TextField.Root>
-
       <TextField.Root>
         <TextField.Slot>
           <RiUserStarLine />
         </TextField.Slot>
-        <TextField.Input placeholder="Email Address" />
+        <TextField.Input placeholder="Email Address" {...register("email")} />
       </TextField.Root>
 
       <Button>Add Lead</Button>
-    </>
+    </form>
   );
 };
 
-export default NewLead;
+export default NewLeadPage;
