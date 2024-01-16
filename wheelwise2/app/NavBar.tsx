7bee432e09781582a9d08a6bb31c121e6159d19c
Danin Na <dnami001@ucr.edu>
2024-01-15 20:00:50 -0800

    Navbar - Done!

---
+++ wheelwise2/app/NavBar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { LiaCarSolid } from "react-icons/lia";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { lable: "Dashboard", href: "/" },
+    { lable: "Leads", href: "/Leads" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b p-5 mb-5 items-center">
+      <Link href="/">
+        <LiaCarSolid />
+      </Link>
+      <ul className="flex space-x-6 items-center">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classNames({
+              "text-rose-400": link.href === currentPath,
+              "text-rose-500": link.href !== currentPath,
+              "hover:text-rose-700 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.lable}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
