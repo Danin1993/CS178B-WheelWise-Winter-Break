f6c70efb44fbd7e071baa304b36e7f19e1e10cdc
Danin Na <dnami001@ucr.edu>
2024-01-18 15:59:52 -0800

    Refactor: Clean Functions

--- wheelwise2/app/components/CallOut.tsx
+++ wheelwise2/app/components/CallOut.tsx
@@ -1,57 +1,61 @@
-import React, { ReactNode, ElementType } from "react";
-import { Box, Callout } from "@radix-ui/themes";
+import React, { ReactNode, ElementType } from 'react'
+import { Box, Callout } from '@radix-ui/themes'
 
 interface Props {
-  children: ReactNode;
-  icon: ElementType;
-  size?: "1" | "2" | "3";
-  variant?: "soft" | "surface" | "outline";
-  color?:
-    | "tomato"
-    | "red"
-    | "ruby"
-    | "crimson"
-    | "pink"
-    | "plum"
-    | "purple"
-    | "violet"
-    | "iris"
-    | "indigo"
-    | "blue"
-    | "cyan"
-    | "teal"
-    | "jade"
-    | "green"
-    | "grass"
-    | "brown"
-    | "orange"
-    | "sky"
-    | "mint"
-    | "lime"
-    | "yellow"
-    | "amber"
-    | "gold"
-    | "bronze"
-    | "gray";
+	children: ReactNode
+	icon: ElementType
+	size?: '1' | '2' | '3'
+	variant?: 'soft' | 'surface' | 'outline'
+	color?:
+	| 'tomato'
+	| 'red'
+	| 'ruby'
+	| 'crimson'
+	| 'pink'
+	| 'plum'
+	| 'purple'
+	| 'violet'
+	| 'iris'
+	| 'indigo'
+	| 'blue'
+	| 'cyan'
+	| 'teal'
+	| 'jade'
+	| 'green'
+	| 'grass'
+	| 'brown'
+	| 'orange'
+	| 'sky'
+	| 'mint'
+	| 'lime'
+	| 'yellow'
+	| 'amber'
+	| 'gold'
+	| 'bronze'
+	| 'gray'
+	title?: string
 }
 
 const CallOut = ({
-  children,
-  icon: Icon,
-  size = "2",
-  variant = "surface",
-  color = "red",
+	children,
+	title,
+	icon: Icon,
+	size = '2',
+	variant = 'surface',
+	color = 'red',
 }: Props) => {
-  return (
-    <Box position="fixed" bottom="0" right="0" left="0" className="p-5">
-      <Callout.Root variant={variant} color={color} size={size}>
-        <Callout.Icon>
-          <Icon />
-        </Callout.Icon>
-        <Callout.Text>{children}</Callout.Text>
-      </Callout.Root>
-    </Box>
-  );
-};
+	return (
+		<Box position='fixed' bottom='0' right='0' left='0' className='p-5'>
+			<Callout.Root variant={variant} color={color} size={size}>
+				<Callout.Icon>
+					<Icon />
+				</Callout.Icon>
+				<Callout.Text>
+					{title && <strong>{title} : </strong>} {children}
+				</Callout.Text>
+			</Callout.Root>
+		</Box>
+	)
+}
 
-export default CallOut;
+export default CallOut
--- wheelwise2/app/leads/new/page.tsx
+++ wheelwise2/app/leads/new/page.tsx
@@ -1,117 +1,104 @@
-"use client";
+'use client'
 
-import { Button, TextField, Box, Callout } from "@radix-ui/themes";
-import { RiUserStarLine, RiUserStarFill } from "react-icons/ri";
-import { IoWarningOutline } from "react-icons/io5";
-import { useForm } from "react-hook-form";
-import React, { useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import CallOut from "@/app/components/CallOut";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createLeadSchema } from "@/app/components/Schemas";
+import { Button, TextField, Box, Callout } from '@radix-ui/themes'
+import { RiUserStarLine, RiUserStarFill } from 'react-icons/ri'
+import { IoWarningOutline } from 'react-icons/io5'
+import { useForm } from 'react-hook-form'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import CallOut from '@/app/components/CallOut'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createLeadSchema } from '@/app/components/Schemas'
+import Spinner from '@/app/components/Spinner'
 
 interface NewLeadForm {
-  firstName: string;
-  lastName: string;
-  phone: string;
-  email: string;
+	firstName: string
+	lastName: string
+	phone: string
+	email: string
 }
 
 const NewLeadPage = () => {
-  const router = useRouter();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<NewLeadForm>({
-    resolver: zodResolver(createLeadSchema),
-  });
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+	const router = useRouter()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<NewLeadForm>({
+		resolver: zodResolver(createLeadSchema),
+	})
+	const [error, setError] = useState('')
+	const [success, setSuccess] = useState('')
+	const [isSubmiting, setIsSubmitting] = useState(false)
 
-  return (
-    <div>
-      {/* Error Handeling - Server Side */}
-      {error && <CallOut icon={IoWarningOutline}>{error}</CallOut>}
-      {success && (
-        <CallOut icon={IoWarningOutline} color="green">
-          {success}
-        </CallOut>
-      )}
+	return (
+		<div>
+			{/* Error Handeling - Client Side */}
 
-      {/* Error Handeling - Client Side */}
-      {(errors.firstName && (
-        <CallOut icon={IoWarningOutline}>
-          First Name : {errors.firstName.message}
-        </CallOut>
-      )) ||
-        (errors.lastName && (
-          <CallOut icon={IoWarningOutline}>
-            Last Name : {errors.lastName.message}
-          </CallOut>
-        )) ||
-        (errors.phone && (
-          <CallOut icon={IoWarningOutline}>
-            Phone Number : {errors.phone.message}
-          </CallOut>
-        )) ||
-        (errors.email && (
-          <CallOut icon={IoWarningOutline}>
-            Email Address : {errors.email.message}
-          </CallOut>
-        ))}
+			{error && <CallOut icon={IoWarningOutline}>{error}</CallOut>}
+			{success && <CallOut icon={IoWarningOutline} color="green">{success}</CallOut>}
 
-      {/* Form - Add New Lead */}
-      <form
-        className="flex gap-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            await axios.post("/api/leads", data);
+			{
+				(errors.firstName && <CallOut icon={IoWarningOutline} title='First Name'>{errors.firstName.message}</CallOut>) ||
+				(errors.lastName && <CallOut icon={IoWarningOutline} title='Last Name'>{errors.lastName.message}</CallOut>) ||
+				(errors.phone && <CallOut icon={IoWarningOutline} title='Phone Number'>{errors.phone.message}</CallOut>) ||
+				(errors.email && <CallOut icon={IoWarningOutline} title='Email Address'>{errors.email.message}</CallOut>)
+			}
 
-            setSuccess("Lead has been added");
+			{/* Form - Add New Lead */}
 
-            setTimeout(() => {
-              router.push("/leads");
-            }, 2000); // 2000 milliseconds delay
-          } catch (error) {
-            setError("Error from API Server side!");
-            setSuccess("");
-          }
-        })}
-      >
-        <TextField.Root>
-          <TextField.Slot>
-            <RiUserStarLine />
-          </TextField.Slot>
-          <TextField.Input
-            placeholder="First Name"
-            {...register("firstName")}
-          />
-        </TextField.Root>
-        <TextField.Root>
-          <TextField.Slot>
-            <RiUserStarFill />
-          </TextField.Slot>
-          <TextField.Input placeholder="Last Name" {...register("lastName")} />
-        </TextField.Root>
-        <TextField.Root>
-          <TextField.Slot>
-            <RiUserStarLine />
-          </TextField.Slot>
-          <TextField.Input placeholder="Phone Number" {...register("phone")} />
-        </TextField.Root>
-        <TextField.Root>
-          <TextField.Slot>
-            <RiUserStarLine />
-          </TextField.Slot>
-          <TextField.Input placeholder="Email Address" {...register("email")} />
-        </TextField.Root>
+			<form
+				className='flex gap-3'
+				onSubmit={handleSubmit(async (data) => {
+					try {
+						await axios.post('/api/leads', data)
 
-        <Button>Add Lead</Button>
-      </form>
-    </div>
-  );
-};
+						setError('')
+						setSuccess('Lead has been added')
+						setIsSubmitting(true)
 
-export default NewLeadPage;
+						setTimeout(() => {
+							router.push('/leads')
+						}, 2000) // 2000 milliseconds delay
+
+					} catch (error) {
+
+						setError('Error from API Server side!')
+						setSuccess('')
+						setIsSubmitting(false)
+
+					}
+				})}>
+				<TextField.Root>
+					<TextField.Slot>
+						<RiUserStarLine />
+					</TextField.Slot>
+					<TextField.Input placeholder='First Name' {...register('firstName')} />
+				</TextField.Root>
+				<TextField.Root>
+					<TextField.Slot>
+						<RiUserStarFill />
+					</TextField.Slot>
+					<TextField.Input placeholder='Last Name' {...register('lastName')} />
+				</TextField.Root>
+				<TextField.Root>
+					<TextField.Slot>
+						<RiUserStarLine />
+					</TextField.Slot>
+					<TextField.Input placeholder='Phone Number' {...register('phone')} />
+				</TextField.Root>
+				<TextField.Root>
+					<TextField.Slot>
+						<RiUserStarLine />
+					</TextField.Slot>
+					<TextField.Input placeholder='Email Address' {...register('email')} />
+				</TextField.Root>
+
+				<Button disabled={isSubmiting}>Add Lead {isSubmiting && <Spinner />}</Button>
+			</form>
+		</div >
+	)
+}
+
+export default NewLeadPage
